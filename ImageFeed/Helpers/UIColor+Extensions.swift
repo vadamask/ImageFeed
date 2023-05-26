56521071c9ad860142929f7d0199193c0a95d5dd
Vadim Shishkov <vadamask@gmail.com>
2023-05-26 22:29:46 +0300

    added launchscreen logo

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Extension+UIColor.swift
+//  ImageFeed
+//
+//  Created by Вадим Шишков on 26.05.2023.
+//
+
+import UIKit
+
+extension UIColor {
+  static let ypBlack = UIColor(red: 26, green: 27, blue: 34, alpha: 1)
+}
